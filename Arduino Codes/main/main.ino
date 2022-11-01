5576365023ca974d0e782db15b9db52c5a95b092
Shanaaz Ahamed <77115237+ShanaazAhamed@users.noreply.github.com>
2022-11-01 14:52:56 +0530

    Arduino codes added

--- Arduino Codes/main/main.ino
+++ Arduino Codes/main/main.ino
@@ -1,33 +1,43 @@
-#include <SoftwareSerial.h>
- 
-SoftwareSerial BTSerial(10, 11);   // RX | TX
-char inputByte; 
-void setup()
-{
-  pinMode(13,OUTPUT);
-  Serial.begin(9600);
-  Serial.println("Enter AT commands:");
-  BTSerial.begin(9600);  // HC-05 default speed in AT command mode
-}
- 
-void loop()
-{
-  // Read from HC05 and send to Arduino
-  if (BTSerial.available()>0){
-    Serial.println("READ");
-    Serial.write(BTSerial.read());
-    inputByte = BTSerial.read();
-    
-    if (inputByte=='Z'){
-      digitalWrite(13,HIGH);
+#include <SoftwareSerial.h> 
+#include <DHT.h>
+
+#define DHTPIN 7     // what pin we're connected to
+#define DHTTYPE DHT22
+
+SoftwareSerial MyBlue(10, 11); // RX | TX 
+DHT dht(DHTPIN, DHTTYPE);
+
+char flag;
+float hum; 
+float temp;
+int moist;
+
+void setup() 
+{   
+  Serial.begin(9600); 
+  MyBlue.begin(9600); 
+  dht.begin();
+  Serial.println("Device Connected");
+} 
+void loop() 
+{ 
+//  delay(1000);
+  hum = dht.readHumidity();
+  temp= dht.readTemperature();
+  moist = analogRead(A0);
+  if (MyBlue.available()){ 
+    flag = MyBlue.read(); 
+    Serial.println(flag);
+    if(flag == 'h'){
+      MyBlue.print("Humidity ");
+      MyBlue.println(hum);
+    }else if(flag == 't'){
+      MyBlue.print("Temperature ");
+      MyBlue.println(temp);
+    }else if(flag == 'm'){
+      MyBlue.print("Soil Mositure ");
+      MyBlue.println(moist);
     }
-    else if (inputByte=='z'){
-      digitalWrite(13,LOW);
-    } 
   }
-  // Read from serial monitor and send to HC05
-  if (Serial.available()>0){
-    BTSerial.write(Serial.read());
-    Serial.println("WRITE");
-  }
-}
+  delay(1000);
+} 
